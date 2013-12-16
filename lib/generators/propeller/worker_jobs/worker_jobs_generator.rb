8c1b597e60ae2dd2cf70fab3d1a2b930bcd15432
Farley Knight <farley@farleyknight.com>
2013-12-16 17:02:56 -0500

    Copied these bits from delayed_job

--- lib/generators/propeller/worker_jobs/worker_jobs_generator.rb
+++ lib/generators/propeller/worker_jobs/worker_jobs_generator.rb
@@ -1,11 +1,18 @@
 require 'rails/generators/active_record'
 
 module Propeller
-  class WorkerJobsGenerator < ActiveRecord::Generators::Base
+  class WorkerJobsGenerator < Rails::Generators::Base
+    include Rails::Generators::Migration
+    extend NextMigrationVersion
+
     source_root File.expand_path('../templates', __FILE__)
 
     def copy_worker_jobs_migration
       migration_template "migration.rb", "db/migrate/worker_jobs_create.rb"
+    end
+
+    def self.next_migration_number dirname
+      ActiveRecord::Generators::Base.next_migration_number dirname
     end
   end
 end
