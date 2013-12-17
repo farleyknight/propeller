4d84e816e81680add288710dbd82a8b9a282a7d5
Farley Knight <farleyknight@gmail.com>
2013-12-16 20:12:13 -0500

    I will get this working.. eventually..

--- lib/generators/propeller/worker_jobs/worker_jobs_generator.rb
+++ lib/generators/propeller/worker_jobs/worker_jobs_generator.rb
@@ -2,7 +2,8 @@ require 'rails/generators'
 require 'rails/generators/active_record'
 
 module Propeller
-  class WorkerJobsGenerator < ActiveRecord::Generators::Base
+  class WorkerJobsGenerator < Rails::Generators::Base
+    include Rails::Generators::Migration
     source_root File.expand_path('../templates', __FILE__)
 
     def copy_worker_jobs_migration
@@ -13,7 +14,7 @@ module Propeller
       invoke "active_record:model", ["worker_job"], :migration => false
     end
 
-    def self.next_migration_number dirname
+    def self.next_migration_number(dirname)
       ActiveRecord::Generators::Base.next_migration_number dirname
     end
   end
